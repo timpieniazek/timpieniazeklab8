9257a351f48020e78453e70e6c0004ad48fd75f6
Tim Pieniazek <timpieniazek@gmail.com>
2018-05-01 22:33:21 -0400

    Made adjustments to Validator, added new overloaded getString method with conditions

--- src/StudentInformation.java
+++ src/StudentInformation.java
@@ -1,48 +1,59 @@
 import java.util.Scanner;
 
-public class StudentInformation {
+// Tim Pieniazek 05/01/2018
+// GCB Java Lab 8
 
-	final static String[] NAMES = { "Amy Tan", "Kim Kardashian", "Carrie Fisher", "Angela Lansbury", "Eleanor Roosevelt",
-			"Fannie Brice", "Debbie Reynolds", "Elizabeth Taylor", "Shirley MacLaine", "Kim Driscoll",
-			"Peter Parker", "Tony Stark", "Stan Lee", "Ayn Rand", "Rand Paul", "Al Franken", "Mike Huckabee",
-			"Mary Jones", "Tim Smith", "Jimmy Johnson" };
+public class StudentInformation {
+	
+	// final static constants for the arrays of student information
+	//**Thank you Vicki Rush for providing these** :)
+	final static String[] NAMES = { "Amy Tan", "Kim Kardashian", "Carrie Fisher", "Angela Lansbury",
+			"Eleanor Roosevelt", "Fannie Brice", "Debbie Reynolds", "Elizabeth Taylor", "Shirley MacLaine",
+			"Kim Driscoll", "Peter Parker", "Tony Stark", "Stan Lee", "Ayn Rand", "Rand Paul", "Al Franken",
+			"Mike Huckabee", "Mary Jones", "Tim Smith", "Jimmy Johnson" };
 	final static String[] HOMETOWNS = { "Dayton, OH", "Los Angeles, CA", "Portland, ME", "Boston, MA", "Seattle, WA",
 			"Chicago, IL", "New York, NY", "Albequerque, NM", "Atlanta, GA", "Dayton, OH", "Los Angeles, CA",
 			"Portland, ME", "Boston, MA", "Seattle, WA", "Chicago, IL", "New York, NY", "Albequerque, NM",
-			"Atlanta, GA", "Dayton, OH", "Los Angeles, CA", "Portland, ME", "Boston, MA", "Seattle, WA",
-			"Chicago, IL", "Dayton, OH", "Los Angeles, CA" };
-	final static String[] FAVORITEFOODS = { "Pad Thai", "Mac & Cheese", "Chicken Soup", "Beef Tartare", "Grilled Cheese",
-			"PopTarts", "Ramen", "Sauerkraut", "Fruit Snacks", "Sandwiches", "Pad Thai", "Mac & Cheese",
-			"Chicken Soup", "Beef Tartare", "Grilled Cheese", "PopTarts", "Ramen", "Sauerkraut", "Fruit Snacks",
-			"Sandwiches" };
-	
+			"Atlanta, GA", "Dayton, OH", "Los Angeles, CA", "Portland, ME", "Boston, MA", "Seattle, WA", "Chicago, IL",
+			"Dayton, OH", "Los Angeles, CA" };
+	final static String[] FAVORITEFOODS = { "Pad Thai", "Mac & Cheese", "Chicken Soup", "Beef Tartare",
+			"Grilled Cheese", "PopTarts", "Ramen", "Sauerkraut", "Fruit Snacks", "Sandwiches", "Pad Thai",
+			"Mac & Cheese", "Chicken Soup", "Beef Tartare", "Grilled Cheese", "PopTarts", "Ramen", "Sauerkraut",
+			"Fruit Snacks", "Sandwiches" };
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
+		
+		// final constants for prompts for use in Validator class methods
 		final String PROMPT_ONE = "(enter a number 1-20): ";
 		final String PROMPT_TWO = "(enter \"hometown\" or \"favorite food\"): ";
-		final String PROMPT_THREE = "Would you like to know more? (enter \"yes\" or \"no\"): ";
-
+		final String PROMPT_THREE = "Would you like to know more? ";
+		final String PROMPT_THREE = "(enter \"yes\" or \"no\"): ";
 		
+		final String HOME_STATEMENT = "\"%n%s is from %s.\", NAMES[studentId].split(\" \")[0], HOMETOWNS[studentId]";
+		final String FOOD_STATEMENT = "%n%s will tell you their favorite food is %s";
 
 		System.out.print("Welcome to our Java class. ");
 
+		// outer while loop conditional for exiting out of program
 		String cont = "y";
 
 		while (cont.equalsIgnoreCase("y")) {
 
 			System.out.print("Which student would you like to learn more about? ");
-			
-			int studentId = Validator.getInt(scan, PROMPT_ONE, 1, 20);
 
-			System.out.printf("%nStudent %s is %s. What would you like to know about %s?", studentId, NAMES[studentId],
-					NAMES[studentId].split(" ")[0]);
-			
+			// calls Validator.getInt to choose a student from NAMES[] with range of 1 - 20
+			int studentId = Validator.getInt(scan, PROMPT_ONE, 1, 20);
+			System.out.printf("%nStudent %s is %s. What would you like to know about %s?",
+					studentId, NAMES[studentId], NAMES[studentId].split(" ")[0]);
+
+			// calls Validator.getString to prompt for information topic (hometown or favorite food)
 			String infoTopic = Validator.getString(scan, PROMPT_TWO);
 
 			if (infoTopic.equalsIgnoreCase("hometown")) {
-
-				System.out.printf("%n%s is from %s.", NAMES[studentId].split(" ")[0], HOMETOWNS[studentId]);
+				// if "hometown" is chosen, prints the hometown statement
+				System.out.printf(HOME_STATEMENT);
+				
 
 			} else if (infoTopic.equalsIgnoreCase("favorite food")) {
 
@@ -51,17 +62,24 @@ public class StudentInformation {
 
 			} else {
 
-				System.out.print("\nThat data does not exist. Please try again. ");
+				
 
 			}
+			
+			System.out.print("Would you like to learn about another student?");
+			
+			cont = Validator.getString(scan, PROMPT_THREE);
 
 		}
 	}
 
 	public static boolean isValidInfoTopic(String topic) {
-		if (topic.equalsIgnoreCase(anotherString))
+		if (topic.equalsIgnoreCase(topic))
+			;
 	}
-	
-	public static String printHometown()
+
+	public static String printHometown() {
+	}
+}
 
 }
